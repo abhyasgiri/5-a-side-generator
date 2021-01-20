27572725a83b48b35f96bb7a46214a3420463b55
Abhyas Giri <abhyasgiri@outlook.com>
2021-01-20 20:51:16 +0000

    app has issues

--- service_1/application/__init__.py
+++ service_1/application/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv 
 
+db = SQLAlchemy(app)
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:////tmp/football_db' 
-app.config["SECRET_KEY"] = "secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "DATABASE_URI"
 db = SQLAlchemy(app)
 
 from application.models import Players
--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -5,16 +5,25 @@ from flask import render_template, flash
 
 @app.route("/")
 def index():
-    league_response = requests.get("http://127.0.0.1:5002/league")
-    pack_response = requests.get("http://127.0.0.1:5003/pack", data=league_response.text)
+    league_response = requests.get("http://football-cards_service-2:5002/league")
+    pack_response = requests.get("http://football-cards_service-3:5003/pack", data=league_response.text)
+
     information = {'league': league_response.json()["league"] , 'pack': pack_response.json()["pack"]}
-    player_response = requests.post("http://127.0.0.1:5004/player", json=information)
+
+    player_response = requests.post("http://football-cards_service-4:5004/player", json=information)
+
     new_player = Players(player_name=player_response.text)
     db.session.add(new_player)
     db.session.commit()
-    return render_template("index.html", new_player=player_response.text, pack=pack_response.text, league=league_response.text)
+
+    all_players = Players.query.all()
+
+    return render_template("index.html", new_player=player_response.text, pack=pack_response.text, league=league_response.text, all_players=all_players)
+
 
 #do logic in service 4
 #player picks a random league, gets a random pack (gold, silver, bronze), 
 #happy hour and normal times - happy hour the chance of getting gold pack is 50%
 #and if you get a silver pack, you get two players
+
+#make the query a bit more selective - 
