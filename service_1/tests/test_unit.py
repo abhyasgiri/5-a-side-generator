c052303bfbc7c20e0def1c9b30c21b7a55301ee3
Abhyas Giri <abhyasgiri@outlook.com>
2021-01-23 01:43:35 +0000

    test service 1

--- service_1/tests/test_unit.py
+++ service_1/tests/test_unit.py
@@ -24,9 +24,9 @@ class TestResponse(TestBase):
 
     def test_one(self):
         with requests_mock.mock() as a:
-            a.get("http://football-cards_service-2:5002/league", text="English")
-            a.get("http://football-cards_service-3:5000/pack", text="Gold")
-            a.post("http://football-cards_service-4:5000/player", text="Pogba")
+            a.get("http://football-cards_nation-backend:5002/league", text="English")
+            a.get("football-cards_pack-backend:5000/pack", text="Gold")
+            a.post("http://football-cards_player-backend/player", text="Pogba")
             response = self.client.get(url_for('index'))
             self.assertIn(b"You have won Pogba from a Gold pack in the English league!", response.data) #need to correct frontend display
             self.assertIn(b"You have won test_player", response.data) #maybe remove this? it tries to check for test_player from above but i dont think that goes through the system as it doesnt have a pack or league
