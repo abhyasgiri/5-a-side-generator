84dc226bb8b469292679cd2d488724e6693f6238
Abhyas Giri <abhyasgiri@outlook.com>
2021-01-23 12:14:43 +0000

    change to ip

--- service_4/tests/test_unit.py
+++ service_4/tests/test_unit.py
@@ -10,12 +10,5 @@ class TestBase(TestCase):
 
 class TestResponse(TestBase):
 
-    def test_get_player(self):
-        with patch("random.choice") as m:
-            m.return_value = "Gold"
-            response = self.client.get(url_for('get_pack'))
-            self.assertIn(b"Gold", response.data)
-
-#        for _ in range(10):
-#            response = self.client.get(url_for('get_pack'))
-#            self.assertIn(response.data, [b"Gold", b"Silver", b"Bronze"])
+    def test_player(self):
+        
