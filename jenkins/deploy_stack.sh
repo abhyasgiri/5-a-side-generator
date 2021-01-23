5a07924dab568ebf74a3850b1ba63ce17f8939a1
Abhyas Giri <abhyasgiri@outlook.com>
2021-01-23 00:59:40 +0000

    tasks

--- jenkins/deploy_stack.sh
+++ jenkins/deploy_stack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
 ssh -i ~/.ssh/id_rsa jenkins@swarm-manager << EOF
     export DATABASE_URI=${DATABASE_URI}
