29dfa966127ebf142ead81a5928401a94e0c2971
Abhyas Giri <abhyasgiri@outlook.com>
2021-01-24 12:10:27 +0000

    service 2 changed

--- service_4/application/routes.py
+++ service_4/application/routes.py
@@ -6,39 +6,39 @@ import random
 def get_player():
     pack = request.json["pack"]
     league = request.json["league"]
-    if league == "English" and pack == "Gold":
+    if league == "ENGLISH" and pack == "Gold":
         players_pack = ["Pogba", "Firmino", "Son", "Kane"]
         player_name = random.choice(players_pack)
         return Response(player_name, mimetype ='text/plain')
-    elif league == "English" and pack == "Silver":
+    elif league == "ENGLISH" and pack == "Silver":
         players_pack = ["Grealish", "Baily", "Neto", "Matip"]
         player_name = random.choice(players_pack)
         return Response(player_name, mimetype ='text/plain')
-    elif league == "English" and pack == "Bronze":
+    elif league == "ENGLISH" and pack == "Bronze":
         players_pack = ["Knight", "Obafemi", "Shabani"]
         player_name = random.choice(players_pack) 
         return Response(player_name, mimetype ='text/plain')
-    elif league == "Spanish" and pack == "Gold":
+    elif league == "SPANISH" and pack == "Gold":
         players_pack = ["Hazard", "Messi", "Suarez", "Joao Felix"]
         player_name = random.choice(players_pack) 
         return Response(player_name, mimetype ='text/plain')
-    elif league == "Spanish" and pack == "Silver":
+    elif league == "SPANISH" and pack == "Silver":
         players_pack = ["Jose Espino", "Alvaro Vallejo", "Sergio Leon"]
         player_name = random.choice(players_pack) 
         return Response(player_name, mimetype ='text/plain')
-    elif league == "Spanish" and pack == "Bronze":
+    elif league == "SPANISH" and pack == "Bronze":
         players_pack = ["Fede Vadillo", "Ximo Navarro", "Mosquera"]
         player_name = random.choice(players_pack) 
         return Response(player_name, mimetype ='text/plain')
-    elif league == "German" and pack == "Gold":
+    elif league == "GERMAN" and pack == "Gold":
         players_pack = ["Jaden Sancho", "Erling Halland", "Robert Lewandowski", "Serge Gnabry"]
         player_name = random.choice(players_pack) 
         return Response(player_name, mimetype ='text/plain')
-    elif league == "German" and pack == "Silver":
+    elif league == "GERMAN" and pack == "Silver":
         players_pack = ["Richter", "Ziegler", "Thomas Friedrich"]
         player_name = random.choice(players_pack) 
         return Response(player_name, mimetype ='text/plain')
-    elif league == "German" and pack == "Bronze":
+    elif league == "GERMAN" and pack == "Bronze":
         players_pack = ["Schmelzer", "Drexler", "Geiger"]
         player_name = random.choice(players_pack) 
         return Response(player_name, mimetype ='text/plain')
